1c7c00f1e3b9953b76569af91c0461709f58d616
Enfrai <shuohui.liu@yahoo.com>
2026-09-13 06:07:56 +1200

    done

--- client/rental-car-app/src/App.tsx
+++ client/rental-car-app/src/App.tsx
@@ -4,8 +4,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-import DashboardPage from './pages/Dashboard';
-import { PathUserLogin, PathUserRegister, PathUserDashboad } from './Config';
+import DashboardPage from './pages/DashboardPage';
+import BookingCarPage from './pages/BookingCarPage';
+import BookingConfirmPage from './pages/BookingConfirmPage';
+import BookingHistoryPage from './pages/BookingHistoryPage';
+import BookingRegisterCarPage from './pages/BookingRegisterCarPage';
+import { PathUserLogin, PathUserRegister, PathUserDashboad, PathBookingCar, PathBookingConfirm, PathBookingHisgory, PathCarRegister, PathCarSearch } from './Config';
 
 function App() {
   return (
@@ -16,6 +20,11 @@ function App() {
           <Route path={PathUserLogin} element={<LoginPage />} />
           <Route path={PathUserRegister} element={<RegisterPage />} />
           <Route path={PathUserDashboad} element={<DashboardPage />} />
+          <Route path={PathCarRegister} element={<BookingRegisterCarPage />} />
+          <Route path={PathBookingHisgory} element={<BookingHistoryPage />} />
+          <Route path={PathBookingConfirm} element={<BookingConfirmPage />} />
+          <Route path={PathBookingCar} element={<BookingCarPage />} />
+          <Route path={PathCarSearch} element={<DashboardPage />} />
         </Routes>
       </BrowserRouter>
     </UserProvider>
---
+++ client/rental-car-app/src/pages/BookingConfirmPage.tsx
@@ -0,0 +1,102 @@
+import '../App.css';
+import { useNavigate } from 'react-router-dom';
+import { useUser } from '../UserContext';
+import { PathBookingCar, PathBookingHisgory, PathBookingConfirm, PathCarRegister } from '../Config';
+
+export default function BookingConfirmPage() {
+    const navigate = useNavigate();
+    const { user } = useUser();
+
+    const handleBookingCar = () => {
+        navigate(PathBookingCar);
+    }
+
+    const handleBookingHistory = () => {
+        navigate(PathBookingHisgory);
+    }
+
+    const handleRegisterCar = () => {
+        navigate(PathCarRegister);
+    }
+
+    const handleBookingConfirm = () => {
+        navigate(PathBookingConfirm);
+    }
+
+    console.log(`is_admin: ${user?.is_admin}, is_customer: ${user?.is_customer}`)
+
+    var role = user?.is_customer ? 'Customer' : '';
+    if (user?.is_admin) {
+        if (role) {
+            role += " & ";
+        }
+        role += 'Admin'
+    }
+
+    console.log(`role: ${role}`)
+
+    return (
+      <section id="center">
+        <div style={{ margin: "0px 24px" }}>
+          <h1>Hello {user?.username}, Role: {role}</h1>
+        </div>
+        {   user?.is_customer ? 
+            <button
+                style={{
+                    width: "180px",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginTop: "124px",
+                }}
+                type="button"
+                className="counter"
+                onClick={handleBookingCar}
+            >
+            Book a car
+            </button> : null
+        }
+        {   user?.is_admin ? 
+            <button
+                style={{
+                    width: "180px",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+                type="button"
+                className="counter"
+                onClick={handleRegisterCar}
+            >
+            Register a car
+            </button> : null
+        }
+        {   user?.is_customer ? 
+            <button
+                style={{
+                    width: "180px",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+                type="button"
+                className="counter"
+                onClick={handleBookingHistory}
+            >
+            Booking history
+            </button> : null
+        }
+        {   user?.is_admin ? 
+            <button
+                style={{
+                    width: "180px",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+                type="button"
+                className="counter"
+                onClick={handleBookingConfirm}
+            >
+            Booking confirm
+            </button> : null
+        }
+      </section>
+    );
+}
